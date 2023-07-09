23831472da1d7693845be386d6947bf1a9bf3e3c
Federico Airoldi <airoldibusiness@gmail.com>
2023-07-09 23:58:41 +0200

    FASE 2 - impostazione navbar

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,17 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBarComp from "./components/NavBarComp.vue"
+
+
 </script>
 
 <template>
-  <div class="col-8">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue"/>
+  <NavBarComp/>
+
 </template>
 
 <style lang="scss">
---
+++ Frontend/src/components/NavBarComp.vue
@@ -0,0 +1,55 @@
+<script>
+    import {store} from '../store'
+    export default {
+
+        name: "NavBarComp",
+        components: {
+            
+        },
+
+        data() {
+            return {
+                store,
+            }
+        },
+
+        methods: {
+
+        }
+    };
+
+</script>
+
+
+<!-- NAVBAR -->
+<template>
+    <div>
+        <nav class="navbar fixed-top bg-body-tertiary">
+            <div class="container-fluid">
+              <!-- Leftside -->
+              <div>
+      
+              </div>
+      
+              <!-- Center Logo -->
+              <div>
+                  <h2 id="logo">AIRO</h2>
+              </div>
+      
+              <!-- Rightside -->
+              <div>
+      
+              </div>     
+            </div>
+        </nav>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+    #logo {
+        font-weight: bolder;
+    }
+
+</style>
